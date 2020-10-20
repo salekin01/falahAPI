9691e7bc6bfffd07500d544505aa16b422492d6b
salekin01 <salekin01@outlook.com>
2020-10-20 23:42:09 +0200

    delete the commit

--- src/main/java/falah/falah_api/config/EmailUtil.java
+++ src/main/java/falah/falah_api/config/EmailUtil.java
@@ -27,7 +27,7 @@ public class EmailUtil {
 
             Transport.send(message, "falahchemnitzteam", "falah2020" );  //pass needed
         } catch (MessagingException ex) {
-            ex.printStackTrace();  //errorlog is needed
+            ex.printStackTrace();
         }
     }
 }
